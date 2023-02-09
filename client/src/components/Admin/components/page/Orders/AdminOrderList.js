1b38c016cb9de4d289c02692765b4ac9dc915536
VeckovN <novaknolewow@gmail.com>
2023-02-09 10:37:43 +0100

    Added AdminOrderList

---
+++ client/src/components/Admin/components/page/Orders/AdminOrderList.js
@@ -0,0 +1,59 @@
+import {useState} from 'react'
+import { convertDate } from '../../../../../utils/Helper';
+
+const AdminOrderList = ({orders, onComfirmOrder}) =>{
+
+    return (
+        <>
+            <li className ='table_header'>
+                {/* <div className='colone col-1'>OR ID</div> */}
+                <div className='colone col-1'>Username</div>
+                <div className='colone col-2'>Email</div>
+                <div className='colone col-3'>Product</div>
+                <div className='colone col-4'>Amount</div>
+                <div className='colone col-5'>TotalPrice</div>
+                <div className='colone col-6'>Status</div>
+                <div className='colone col-7'>Created</div>
+            </li>
+            {orders.map(order =>{
+                console.log("ORDE:"+ order);
+                    let totalPrice =0;
+                    const completed = order.isCompleted ? 'table-row completed' : 'table-row'
+                    // return <li className='table-row' key={order._id}>
+                    return <li className={completed}  key={order._id}>
+                                {/* {!order.isCompleted && <button className="comfirm_button" onClick={onComfirmOrder(order._id)}>Comfirm</button>} */}
+                                {!order.isCompleted && <button className="comfirm_button" onClick={() =>{onComfirmOrder(order._id, order.user.email);}}>Comfirm</button>}
+                                
+                                <div className='colone col-1'>
+                                    {/* {order._id} */}
+                                    {order.user ? order.user.username : "username"}
+                                </div>
+                                <div className='colone col-2'>
+                                    {order.user ? order.user.email : "emailAdr@gmail.com"}
+                                </div>
+                                <div className ='colone col-3'>
+                                    {order.products.map(prod =>{
+                                    return <p>{prod.product.name}</p>
+                                    })}
+                                </div>
+                                <div className ='colone col-4'>  
+                                        {order.products.map(prod =>{
+                                            totalPrice+=prod.amount * prod.product.price;
+                                            return <p>{prod.amount}</p>
+                                        })}
+                                    {/* {order.products.map(prod =>{
+                                        totalAmount+=prod.product.amount * prod.product.price;
+                                        <p>{prod.product.amount}</p>
+                                    })} */}
+                                </div>
+                                <div className ='colone col-5'>{totalPrice}</div>
+                                <div className ='colone col-6'>{order.isCompleted ? <p>Completed</p> : <p>Pending</p>}</div>
+                                <div className ='colone col-7'><div className='dateLabel'>{convertDate(order.createdAt).day} {convertDate(order.createdAt).hour}</div></div>
+                        </li>
+                })}
+            </>
+
+    )
+}
+
+export default AdminOrderList
