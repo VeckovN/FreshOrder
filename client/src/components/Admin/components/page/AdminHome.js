2d85e4fa3539f3cebb011873b0eebebf00651d39
VeckovN <novaknolewow@gmail.com>
2023-02-10 21:43:21 +0100

    AdminOrder modified (css)

--- client/src/components/Admin/components/page/AdminHome.js
+++ client/src/components/Admin/components/page/AdminHome.js
@@ -1,6 +1,4 @@
 import {useState} from 'react'
-
-import Leaderboard from './Leaderboard.js'
 // import Users from './Users/Users.js'
 import AdminOrders  from './Orders/AdminOrders.js'
 
@@ -11,7 +9,6 @@ const AdminHome = (props) =>{
     return(
 
         <main className='mainAdmin'>
-            <Leaderboard />
             {/* this handler show modal (userModal) adminUserModalUpdate */}
             {/* onEnterOrderDeliveryTime(orderID) from AdminOrders */}
             <AdminOrders onEnterOrderDeliveryTime={props.onOrderDeliveryTime}/>
