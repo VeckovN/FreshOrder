db0b25067e4125b9b06c1716cd884580fe096f12
VeckovN <novaknolewow@gmail.com>
2023-05-22 22:12:04 +0200

    Seperated component for render Cart Modal

---
+++ client/src/components/Cart/CartModal.js
@@ -0,0 +1,43 @@
+import Modal from '../UI/Modal/Modal';
+
+const CartModal = ({itemsList,cartItems,totalAmount,orderCartItems, onClose, onCloseSignClick }) =>{
+
+    //MODAL STRUCTURE
+    const cartHeaderContext =
+            'FreshOrder';
+
+    const cartBodyContext =
+            //props.children
+        cartItems.length > 0      
+            ? itemsList
+            : <div className='no_items'>No Items in cart</div> 
+    
+    const cartFooterContext = 
+            <>
+                <div className='total_amount'>
+                    TotalAmont: <span>{totalAmount}</span>
+                </div>
+                <div className='order'>
+                    <button onClick={orderCartItems} id='Order'>Order</button>
+                </div>
+            </>
+
+    console.log("CARTTT");
+    return (
+        //this onCloseModal handler is in parrent of Modal
+        <Modal 
+            HeaderContext = {cartHeaderContext}
+            BodyContext = {cartBodyContext}
+            FooterContext = {cartFooterContext}
+            onCloseModal={onClose} 
+            onCloseSignClick={onCloseSignClick}>
+            {/* {cartItems.length >0      
+                ? ItemsList
+                : <div className='no_items'>No Items in cart</div> 
+            } */}
+        </Modal>
+    )
+
+}
+
+export default CartModal
