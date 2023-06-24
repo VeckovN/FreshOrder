6957025ec80d2a72d2e38f6cd4bc12a5ce9c235c
VeckovN <novaknolewow@gmail.com>
2023-06-24 09:41:00 +0200

    Font and error message added

--- client/src/components/Admin/components/page/Products/AddProduct/AddProductForm.js
+++ client/src/components/Admin/components/page/Products/AddProduct/AddProductForm.js
@@ -2,14 +2,13 @@ const AddProductForm = ({values, errors, categoryOptions, removeSelectedImage, h
 
     return (
         <div className='addProduct_container'>
-            <h2>Add Product</h2>
+            {/* <h2>Add Product</h2> */}
             {/* <form className='addProduct_form' onSubmit={onSubmitAddProduct}>   */}
             <form className='addProduct_form' onSubmit={handleProductSubmit}>  
 
                 <div className='addProduct_inputFields'>
                     <div className='addProduct_input'>
                         <label>Category</label>
-                        {errors.category && <p>Select a category</p>}
                         {/* <select name='category' value={category} defaultValue={''} onChange={onChangeCategorySelect}> */}
                         <select name='category' value={values.category} defaultValue={''} onChange={handleChanges}>
                             <option value=''  disabled> Select Category </option>
@@ -17,6 +16,7 @@ const AddProductForm = ({values, errors, categoryOptions, removeSelectedImage, h
                                 return <option values={category}>{category}</option>
                             })}
                         </select>
+                        {errors.category && <p className='error_product'>Select a category</p>}
                     </div>
 
                     <div className='addProduct_input'>
@@ -28,7 +28,7 @@ const AddProductForm = ({values, errors, categoryOptions, removeSelectedImage, h
                             value={values.product_name}
                             placeholder='Enter product name'
                         />
-                        {errors.product_name && <p>{errors.product_name}</p>}
+                        {errors.product_name && <p className='error_product'>{errors.product_name}</p>}
                     </div>
 
                     <div className='addProduct_input'>
@@ -41,7 +41,7 @@ const AddProductForm = ({values, errors, categoryOptions, removeSelectedImage, h
                             value={values.product_price}
                             placeholder='Enter product price'
                         />
-                        {errors.product_price && <p>{errors.product_price}</p>}
+                        {errors.product_price && <p className='error_product'>{errors.product_price}</p>}
                     </div>
                     <div className='addProduct_input'>
                         <label>Description</label>
@@ -53,14 +53,14 @@ const AddProductForm = ({values, errors, categoryOptions, removeSelectedImage, h
                             onChange={handleChanges} 
                             value={values.product_description}
                         />
-                        {errors.product_description && <p>{errors.product_description}</p>}
+                        {errors.product_description && <p className='error_product'>{errors.product_description}</p>}
                     </div>
                 </div>
 
                 <div className='addProduct_images'>
                     <div className='addProduct_input'>
                         <label>Image</label>
-                        {errors.image && <p>Import a image</p>}
+                        {errors.image && <p className='error_product'>Import a image</p>}
                         <input type="file" name="image" onChange={handleChanges} />
                     </div>
 
