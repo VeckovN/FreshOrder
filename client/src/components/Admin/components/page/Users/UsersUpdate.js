aebd9adb2c41701488e1f9320ff3007e410e3b1a
VeckovN <novaknolewow@gmail.com>
2023-07-13 18:07:49 +0200

    User profil update Fixed

--- client/src/components/Admin/components/page/Users/UsersUpdate.js
+++ client/src/components/Admin/components/page/Users/UsersUpdate.js
@@ -1,13 +1,12 @@
 import {useState, useContext, useRef, useEffect} from 'react';
 import { useLocation , useNavigate} from 'react-router-dom';
+import useForm from '../../../../../utils/Hooks/useForm';
 import axios from 'axios';
+
 
 import notificationContext from '../../../../Store/notification-context';
 
 const UsersUpdate = () =>{
-
-    //Take passed state through Link component (<Link state={}>)
-
     const navigate = useNavigate();
 
     const [data, setData] = useState();
@@ -15,6 +14,56 @@ const UsersUpdate = () =>{
 
     const location = useLocation();
     const userInfo = location.state;
+
+    const initialInputObject = {
+        username:"", 
+        email:"", 
+        address:"", 
+        phone_number:"" 
+    }
+
+    
+
+    const {values, errors, handleChanges, resetAllValues} = useForm(initialInputObject);
+
+    const onUserUpdate = async() =>{
+        let updateObject = {};
+        if(values.username !='')
+            updateObject.username = values.username;
+        if(values.email !='')
+            updateObject.email = values.email;
+        if(values.address !='')
+            updateObject.address = values.address;
+        if(values.phone_number !='')
+            updateObject.phone_number = values.phone_number;
+
+        console.log("Updated Object: " + JSON.stringify(updateObject));
+
+        if(Object.keys(updateObject).length != 0){
+            try{
+                const result = await axios.put(`http://localhost:8800/api/users/updateuser/${userInfo._id}`, updateObject)
+                const responseData = result.data;
+                setData(responseData)
+                addSuccess("User successfully updated");
+                updateObject = {};
+                resetAllValues(); //Values state reseted
+
+
+            }
+            catch(err){
+                console.log("Error: " + err);
+                if(err.response.data)
+                    addError(err.response.data.message);
+                else
+                    addError("You can't update right now")
+
+            }
+        }
+        else
+            addError("Not eneterd values");
+        
+    }
+
     //we wanna reRender(show new State) after user update (fetch again new data)
     useEffect(()=>{
         console.log("HHHHHH");
@@ -22,70 +71,8 @@ const UsersUpdate = () =>{
         setLoading(true);
     },[]);
 
-
-    // setUserInfo(location.state);
-
     const {addSuccess, addError} = useContext(notificationContext);
 
-    // const [updatedData, setUpdatedData] = useState({
-    //     username:'',
-    //     email:'',
-    //     address:'',
-    //     phone_number:''
-    // })
-    // const {username, email, address, phone_number} = updatedData;
-
-    // console.log("STATE: \n" + JSON.stringify(updatedData));
-
-    // const onChangeUpdateData = (event)=>{
-    //     const name = event.target.name;
-    //     const value = event.target.value;
-    //     setUpdatedData(prev => ({
-    //         ...prev,
-    //         [name]: value
-    //     }))
-    // }
-    const usernameRef = useRef('');
-    const emailRef = useRef('');
-    const addressRef = useRef('');
-    const phone_numberRef = useRef('');
-
-    const commitHandler = async() =>{
-
-        const username = usernameRef.current.value;
-        const email = emailRef.current.value;
-        const address = addressRef.current.value;
-        const phone_number = phone_numberRef.current.value;
-
-        let updateObject = {};
-        if(username !='')
-            updateObject.username = username;
-        if(email !='')
-            updateObject.email = email;
-        if(address !='')
-            updateObject.address = address;
-        if(phone_number !='')
-            updateObject.phone_number =phone_number;
-      
-
-        console.log("Updated Object: " + JSON.stringify(updateObject));
-
-        try{
-            const result = await axios.put(`http://localhost:8800/api/users/updateuser/${userInfo._id}`, updateObject)
-            const responseData = result.data;
-            setData(responseData)
-            addSuccess("Sucess Updated");
-
-            usernameRef.current.value='';
-            emailRef.current.value='';
-            addressRef.current.value='';
-            phone_numberRef.current.value='';
-        }
-        catch(err){
-            console.log("Error: " + err);
-            addError("You can't update right now")
-        }
-    }
 
     const deleteUserHandler = async() =>{
         try{
@@ -115,10 +102,10 @@ const UsersUpdate = () =>{
                             type='text'
                             name='username'
                             placeholder='Enter new username'
-                            ref={usernameRef}
-                            // value={username}
-                            // onChange={onChangeUpdateData}
+                            onChange={handleChanges}
+                            value={values.username}
                         />
+                        {errors.username && <label className='errorInputLabel'>{errors.username}</label>}
                     </div>
 
                     <div className='input_field users_input_field'>
@@ -129,10 +116,11 @@ const UsersUpdate = () =>{
                             type='text'
                             name='email'
                             placeholder='Enter new email'
-                            ref={emailRef}
-                            // value={email}
-                            // onChange={onChangeUpdateData}
+                            onChange={handleChanges}
+                            value={values.email}
+                            
                         />
+                        {errors.email && <label className='errorInputLabel'>{errors.email}</label>}
                     </div>
 
                     <div className='input_field users_input_field'>
@@ -143,10 +131,10 @@ const UsersUpdate = () =>{
                             type='text'
                             name='address'
                             placeholder='Enter new Address'
-                            ref={addressRef}
-                            // value={address}
-                            // onChange={onChangeUpdateData}
+                            onChange={handleChanges}
+                            value={values.address}
                         />
+                        {errors.address && <label className='errorInputLabel'>{errors.address}</label>}
                     </div>
 
                     <div className='input_field users_input_field'>
@@ -157,16 +145,16 @@ const UsersUpdate = () =>{
                             type='number'
                             name='phone_number'
                             placeholder='Enter new phone number'
-                            ref={phone_numberRef}
-                            // value={phone_number}
-                            // onChange={onChangeUpdateData}
+                            onChange={handleChanges}
+                            value={values.phone_number}
                         />
+                        {errors.phone_number && <label className='errorInputLabel'>{errors.phone_number}</label>}
                     </div>
 
 
                     <div className='users_commit_container'>
                         {/* <button onClick={commitHandler} disabled={!checkForCommit()} className='commitButton'>Commit</button> */}
-                        <button onClick={commitHandler} className='commitButton'>Commit</button>
+                        <button onClick={onUserUpdate} className='commitButton'>Commit</button>
     
                     </div>
                     <div className='users_delete_container'>
