d877feea5e58e4121a60a626e62d52abdcc536e1
VeckovN <novaknolewow@gmail.com>
2023-07-18 18:54:45 +0200

    Authentication Middleware Added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,24 +1,34 @@
 import express from 'express'
 const router = express.Router();
 import {createOrder, completeOrder, getOrder, getOrders, getAllUserOrders, deleteOrder, getOrdersCount }from '../controllers/orderController.js'
-import { verifyAdmin, verifyUser } from '../utility/verifyToken.js';
+import { verifyAdmin, verifyUser, verUser, verAdmin } from '../utility/verifyToken.js';
 
 //RRR router.post('/', verifyUser,createOrder);
-router.post('/', createOrder);
+// router.post('/', createOrder);
+router.post('/', verUser,createOrder);
 //Complete Order
 // router.put('/complete/:id', verifyAdmin, completeOrder);
-router.put('/complete/:id', completeOrder);
+// router.put('/complete/:id', completeOrder);
+router.put('/complete/:id', verUser, completeOrder);
 router.get('/count', getOrdersCount)
+
 //GetOne
-router.get('/:id', verifyUser, getOrder);
+// router.get('/:id', verifyUser, getOrder);
+router.get('/:id', verUser, getOrder);
 //GetAll\
 //RRRRR router.get('/', verifyAdmin, getOrders);
-router.get('/',  getOrders);
+// router.get('/',  getOrders);
+router.get('/', verAdmin, getOrders);
+
 // router.post('/orders', )
 //RRRRR router.get('/userOrders/:userID', verifyUser, getAllUserOrders);
-router.get('/userOrders/:userID', getAllUserOrders);
+// router.get('/userOrders/:userID', getAllUserOrders);
+router.get('/userOrders/:userID',verUser, getAllUserOrders);
+
+
 // router.delete('/:id', verifyAdmin, deleteOrder);
-router.delete('/:idOrder/:idClient',  deleteOrder);
+// router.delete('/:idOrder/:idClient',  deleteOrder);
+router.delete('/:idOrder/:idClient',  verUser, deleteOrder);
 
 
 export default router
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 import {updateUser, deleteUser, getUser, getUsers, getUserCount} from '../controllers/userController.js'
 
-import {verifyUser, verifyAdmin} from '../utility/verifyToken.js';
+import {verifyUser, verifyAdmin, verifyToken, verUser, verAdmin} from '../utility/verifyToken.js';
 
 const router = express.Router();
 
@@ -10,21 +10,35 @@ const router = express.Router();
 //app.use("/users", userRoute); 
 
 //RRRRR router.put('/updateuser/:id', verifyAdmin, updateUser);
-router.put('/updateuser/:id', updateUser);
+// router.put('/updateuser/:id', updateUser);
+router.put('/updateuser/:id', verUser, updateUser);
 
 //only logged user(btw admin as well,because this verifyUser give also
 // permision to admin) can change your account 
-router.put('/:id', verifyUser ,updateUser);
+// router.put('/:id', verifyUser ,updateUser);
+router.put('/:id', verAdmin ,updateUser);
+
 //delete --- api/products/:id
 // router.delete('/:id', verifyUser, deleteUser);
 router.delete('/:id', deleteUser);
+router.delete('/:id', verUser, deleteUser);
+
 //get himself(information) to show self info in fromnted
-router.get('/count', getUserCount);
+// router.get('/count', getUserCount);
+router.get('/count', verUser ,getUserCount);
+
 //RRRRR router.get('/:id', verifyUser, getUser);
-router.get('/:id', getUser);
+// router.get('/:id', getUser);
+
+// router.get('/:id', verifyUser, getUser);
+router.get('/:id', verUser, getUser);
+
 //But only admin could see all users
 // AAAAAAAA router.get('/', verifyAdmin, getUsers);
-router.get('/',  getUsers);
+// router.get('/',  getUsers);
+
+// router.get('/', verifyAdmin, getUsers);
+router.get('/', verAdmin, getUsers);
 
 
 export default router
