38952e7bafdb2528f9b4585a4c018ef564bd7b63
VeckovN <novaknolewow@gmail.com>
2024-06-22 11:52:27 +0200

    Address column added and products amount merged with product name

--- client/src/admin/page/Orders/AdminOrderList.js
+++ client/src/admin/page/Orders/AdminOrderList.js
@@ -6,14 +6,16 @@ const AdminOrderList = ({orders, onConfirmOrder}) =>{
             <li className ='table_header'>
                 <div className='colone col-1'>Username</div>
                 <div className='colone col-2'>Email</div>
-                <div className='colone col-3'>Product</div>
-                <div className='colone col-4'>Amount</div>
+                <div className='colone col-3'>Address</div>
+                <div className='colone col-4'>Product</div>
                 <div className='colone col-5'>Total</div>
                 <div className='colone col-6'>Status</div>
                 <div className='colone col-7'>Created</div>
             </li>
             {orders.map(order =>{
-                    let totalPrice =0;
+                    const totalPrice = order.products.reduce((total, prod) =>{
+                        return total + (prod.amount * (prod.product?.price || 0));
+                    }, 0);
                     const completed = order.isCompleted ? 'table-row completed' : 'table-row'
                     return <li className={completed}  key={order._id}>
                                 {!order.isCompleted && <button className="confirm_button" onClick={() =>{onConfirmOrder(order._id, order.user.email);}}>Confirm</button>}
@@ -25,16 +27,13 @@ const AdminOrderList = ({orders, onConfirmOrder}) =>{
                                 <div className='colone col-2'>
                                     {order.user ? order.user.email : "emailAdr@gmail.com"}
                                 </div>
-                                <div className ='colone col-3'>
-                                    {order.products.map(prod =>{
-                                    return <p>{prod.product!=null ? prod.product.name : 'Deleted Product'}</p>
-                                    })}
+                                <div className ='colone col-3'>  
+                                    {order.user ? order.user.address : "address"}
                                 </div>
-                                <div className ='colone col-4'>  
-                                        {order.products.map(prod =>{
-                                            totalPrice+=prod.amount * prod.product?.price;
-                                            return <p>{prod.amount}</p>
-                                        })}
+                                <div className ='colone col-4'>
+                                    {order.products.map(prod =>{
+                                    return <p>{prod.product!=null ? `${prod.product.name}  x${prod.amount}` : 'Deleted Product'}</p>
+                                    })}
                                 </div>
                                 <div className ='colone col-5'>{totalPrice.toFixed(2)}</div>
                                 <div className ='colone col-6'>{order.isCompleted ? <p>Completed</p> : <p>Pending</p>}</div>
