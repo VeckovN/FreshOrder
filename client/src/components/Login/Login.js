0a31cfc974668102e2a09e5072d0500f3240cb56
VeckovN <novaknolewow@gmail.com>
2024-06-29 19:44:57 +0200

    unnecessry coments removed and useEffect timeout cleanup properly used

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -19,7 +19,7 @@ const Login = () =>{
     }
     const {values, errors, handleChanges, setEmptyFieldError } = useForm(initialInputObject)
     //another way to DispatchAction without defined function in auhtContext like in cartContext
-    const {loading,dispatchAction, loggin} = useContext(authContext);
+    const {loading, dispatchAction, loggin} = useContext(authContext);
     const {addSuccess, addError} = useContext(notificationContext);
     const {closeModal, showRegister} = useContext(modalContext);
 
@@ -32,17 +32,12 @@ const Login = () =>{
             addError(error.message);
         }
         else{
-            //Loading before we fetch user from DB
             dispatchAction({type:"LOGIN_START"})
             try{
                 const res = await  axios.post('http://localhost:8800/api/auth/login', {email: values.email, password:values.password })
-                //Moved this to useEffect 
-                //set timer(to show animation little bit longer) before we change loading to false
                 timerRef.current = setTimeout(()=>{
                     loggin(res.data);
-                    console.log("RES DATA: " + res.data._id);
                     closeModal();
-                    // props.onClose();
                     addSuccess("You successfuly logged")
                 }, 1000);
             }
@@ -63,18 +58,18 @@ const Login = () =>{
         }
     }
 
-
-    //With useREf
     useEffect( () =>{
         //clear the inteval on component unmount
-        return() => clearTimeout(timerRef.current);
+        return() => {
+            if(timerRef.current)
+                clearTimeout(timerRef.current);
+        }
     },[])
 
 
     //When is modal closed set error,loading and user to initiated value(false,null,null)
     const onCloseLoginModal =()=>{
         dispatchAction({type:"RESET"});
-        // props.onClose();
         closeModal()
     }
 
