69f9f3808f29f1f31fe06eb0cf52745519e7dc4c
VeckovN <novaknolewow@gmail.com>
2024-07-01 12:18:01 +0200

    uncomment sendmail logic

--- api/utility/email.js
+++ api/utility/email.js
@@ -3,46 +3,33 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-// export const sendEmail = (sendObj) =>{
-//     const {email, text} = sendObj;
-//     //https://stackoverflow.com/questions/45478293/username-and-password-not-accepted-when-using-nodemailer
+export const sendEmail = (sendObj) =>{
+    const {email, text} = sendObj;
 
-//     // let transporter = nodemailer.createTransport({
-//     //     host:"smtp.gmail.com",
-//     //     port: 465,
-//     //     secure:true,
-//     //     auth:{
-//     //         user: 'orderapp2023@gmail.com',
-//     //         pass: 'OrderNowApp2023',
-//     //     }
-//     //     // },
-//     //     // from: process.env.EMAIL_USERNAME
-//     // })
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:process.env.EMAIL_USERNAME,
+            pass:process.env.EMAIL_PASSWORD
+        }
+    })
 
-//     let transporter = nodemailer.createTransport({
-//         service:'gmail',
-//         auth:{
-//             user:process.env.EMAIL_USERNAME,
-//             pass:process.env.EMAIL_PASSWORD
-//         }
-//     })
+    const message = {
+        from:process.env.EMAIL_USERNAME,
+        to:email,
+        subject:'Order from OrderApp',
+        text:text
+    }
 
-//     const message = {
-//         from:process.env.EMAIL_USERNAME,
-//         to:email,
-//         subject:'Order from OrderApp',
-//         text:text
-//     }
-
-//     transporter.sendMail(message, (err,info)=>{
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             console.log(info);
-//         }
-//     })
-// }
+    transporter.sendMail(message, (err,info)=>{
+        if(err){
+            console.error(err);
+        }
+        else{
+            console.log(info);
+        }
+    })
+}
 
 export const transporter = nodemailer.createTransport({
     service:'gmail',
